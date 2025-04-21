a396c1b3a47251a1282110c1bab604545bcf1492
cmeynet <clemence.meynet14@gmail.com>
2025-04-21 12:22:20 +0200

    Create main.h

---
+++ test_shell/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+extern char **environ;
+char *_getenv(const char *name);
+char **split_line(const char *str, const char *delim);
+void free_tokens(char **tokens);
+int execve_command(char **tokens, char **environ);
+
+#endif
