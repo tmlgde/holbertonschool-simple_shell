d6748013a99f23c777e28a73e60d8fe09b98e553
cmeynet <clemence.meynet14@gmail.com>
2025-04-21 19:22:01 +0200

    add new prototypes to handle PATH to main.h

--- test_shell/main.h
+++ test_shell/main.h
@@ -16,6 +16,7 @@ extern char **environ;
 char *_getenv(const char *name);
 char **split_line(const char *str, const char *delim);
 void free_tokens(char **tokens);
-int execve_command(char **tokens, char **environ);
+int execve_command(char *command_path, char **argv, char **environ);
+char *find_command_path(char *command);
 
 #endif
