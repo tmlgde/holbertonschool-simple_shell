d9b01d0e6f2bae424b54f7f2e550cf315a43fd48
tmlgde <tom.lagarde33@outlook.fr>
2025-04-21 16:18:10 +0000

    delete _path.c and create path.c

--- path.c
+++ path.c
@@ -1,40 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
+
 /**
- * main - Prints all directories listed in the PATH environment variable
+ * find_command_path - Function that searches for a command
+ * in the directories listed in PATH.
+ * @command: The name of the command to find.
  *
- * Return: 0 on success, 1 on failure
- */
-int main(void)
+ * Return: A pointer to a newly allocated string with the full path to the
+ * command if found, or NULL if the command is not found or on failure.
+*/
+char *find_command_path(char *command)
 {
-	char *path = getenv("PATH");
+	char *path = _getenv("PATH");
+	char **access_path = split_line(path, ":");
+	struct stat st;
+	int i;
+	char *full_path = NULL;
+	size_t len = 0;
 
-	if (path == NULL)
-	{
-		printf("La variable PATH n'existe pas.\n");
-		return (1);
+		if (!access_path || !command)
+		return (NULL);
+
+		for (i = 0; access_path[i] != NULL; i++)
+		{
+		/*1 for '/' and 1 for '\0'*/
+		len = strlen(access_path[i]) + strlen(command) + 2;
+
+		full_path = malloc(len * sizeof(char));
+		if (!full_path)
+		{
+			free_tokens(access_path);
+			return (NULL);
+		}
+		/*Copy access_path[i] in full_path*/
+		strcpy(full_path, access_path[i]);
+		strcat(full_path, "/");
+		strcat(full_path, command);
+
+		if (stat(full_path, &st) == 0)
+		{
+			free_tokens(access_path);
+			return (full_path); /*Command found*/
+		}
+		free(full_path);
+		full_path = NULL;
 	}
-
-	else
-		printf("La valeur complete de PATH est:\n%s\n\n", path);
-
-	char *path_copy = strdup(path);
-
-	if (path_copy == NULL)
-	{
-		printf("copie echouee\n");
-		return (1);
-	}
-
-	char *directories = strtok(path_copy, ":");
-
-	while (directories != NULL)
-	{
-		printf("- %s\n", directories);
-		directories = strtok(NULL, ":");
-	}
-
-	free(path_copy);
-	return (0);
+	free_tokens(access_path);
+	return (NULL); /*Command not found*/
 }
